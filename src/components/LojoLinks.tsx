e3409a20d2f73b17b32ccb144adba59cf9482a32
Login Jones <login.jones@gmail.com>
2024-12-01 17:19:34 -0500

    add lojolinks title

--- src/components/LojoLinks.tsx
+++ src/components/LojoLinks.tsx
@@ -6,31 +6,34 @@ const LojoLinks: React.FC = () => {
   return (
     <Space className="lojo-links">
       <div className="social-icons-container">
-        <Popover content="Logins CV">
-            <a href="https://lojones.github.io/resume.html">
-              <img src={`ll-cv.png`} alt="CV" className="link-icon" />
-            </a>
-        </Popover>
-        <Popover content="Chat with Login AI">
-            <a href="https://chat.loginjones.com">
-                <img src={`ll-chat.png`} alt="Chat" className="link-icon" />
-            </a>
-        </Popover>
-        <Popover content="Namesmith - An App to help you do themed naming">
-            <a href="https://namesmith.loginjones.com">
-                <img src={`ll-namesmith.png`} alt="Namesmith" className="link-icon" />
-            </a>
-        </Popover>
-        <Popover content="Logins Youtube playlists">
-            <a href="https://www.youtube.com/@loginjones/playlists">
-                <img src={`ll-youtube.png`} alt="Youtube" className="link-icon" />
-            </a>
-        </Popover>
-        <Popover content="Logins Tiktok playlists">
-            <a href="https://www.tiktok.com/@realloginjones/collection/AI-7183808917036649222">
-                <img src={`ll-tiktok.png`} alt="Tiktok" className="link-icon" />
-            </a>
-        </Popover>
+        <div className="container-label">Logins links</div>
+        <div className="icons-row">
+          <Popover content="Logins CV">
+              <a href="https://lojones.github.io/resume.html">
+                <img src={`ll-cv.png`} alt="CV" className="link-icon" />
+              </a>
+          </Popover>
+          <Popover content="Chat with Login AI">
+              <a href="https://chat.loginjones.com">
+                  <img src={`ll-chat.png`} alt="Chat" className="link-icon" />
+              </a>
+          </Popover>
+          <Popover content="Namesmith - An App to help you do themed naming">
+              <a href="https://namesmith.loginjones.com">
+                  <img src={`ll-namesmith.png`} alt="Namesmith" className="link-icon" />
+              </a>
+          </Popover>
+          <Popover content="Logins Youtube playlists">
+              <a href="https://www.youtube.com/@loginjones/playlists">
+                  <img src={`ll-youtube.png`} alt="Youtube" className="link-icon" />
+              </a>
+          </Popover>
+          <Popover content="Logins Tiktok playlists">
+              <a href="https://www.tiktok.com/@realloginjones/collection/AI-7183808917036649222">
+                  <img src={`ll-tiktok.png`} alt="Tiktok" className="link-icon" />
+              </a>
+          </Popover>
+        </div>
       </div>
     </Space>
   );
